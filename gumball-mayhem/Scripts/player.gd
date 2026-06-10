33357d0fb8e618fc4a9bc67e25726034136e8a3c
Nathan184-jpg <nnagaratnam2@students.mags.school.nz>
2026-06-10 13:13:43 +1200

    added additional double jump

--- gumball-mayhem/Scripts/player.gd
+++ gumball-mayhem/Scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -370.0
+const SPEED = 100.0
+const JUMP_VELOCITY = -250.0
 
 
 
@@ -16,11 +16,18 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	elif is_on_floor() and not double_jump:
+		double_jump = true
+
 	
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept"):
 		if is_on_floor() or is_on_wall():
 			velocity.y = JUMP_VELOCITY 
+		elif double_jump:
+			velocity.y = JUMP_VELOCITY
+			double_jump = false  
+
 			
 	if Input.is_action_pressed("ui_down"): 
 		position.y += 1 
