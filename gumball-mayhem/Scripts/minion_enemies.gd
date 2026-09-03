9f672947198ddc8f7f63491f7aff9a36ef5746ff
Nathan184-jpg <nnagaratnam2@students.mags.school.nz>
2026-09-04 10:18:03 +1200

    Added hit flash to mini enemy

--- gumball-mayhem/Scripts/minion_enemies.gd
+++ gumball-mayhem/Scripts/minion_enemies.gd
@@ -5,9 +5,8 @@ var direction = -1
 
 @export var speed = 300
 @export var player: CharacterBody2D
-
-
-
+@export var anim: AnimationPlayer
+@export var enemy: Sprite2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass;
@@ -23,9 +22,16 @@ func _physics_process(delta: float) -> void:
 func take_damage() -> void:
 	if health > 1: 
 		health -= 1
-		
+
 	else: 
 		queue_free()
+		
+	enemy.modulate = Color.BLUE 
+	await get_tree().create_timer(0.05).timeout 
+	enemy.modulate = Color.WHITE
+
+
+		
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		body.take_damage()
