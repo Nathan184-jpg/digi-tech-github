07a6731d2e3ad925f29803e5f7307c9c3e67d37b
Nathan184-jpg <nnagaratnam2@students.mags.school.nz>
2026-09-05 17:29:24 +1200

    Added pause symbol
    Added pause symbol aswell so that the player can use both esc key and click on the pause icon to access the pause menu.

--- gumball-mayhem/Scripts/pause_menu.gd
+++ gumball-mayhem/Scripts/pause_menu.gd
@@ -1,15 +1,24 @@
 extends Control
 
+@onready var pause_pillar_1 = $/root/Node2D/CanvasLayer/Control/PauseButton/PausePillar
+@onready var pause_pillar_2 = $/root/Node2D/CanvasLayer/Control/PauseButton/PausePillar2
+@onready var pause_icon_pillar = $/root/Node2D/CanvasLayer/Control/PauseButton/PauseIconPillar
 func resume():
 	get_tree().paused = false
 	hide()	
 	$AnimationPlayer.play_backwards("blur")
+	pause_pillar_1.show()
+	pause_pillar_2.show()
+	pause_icon_pillar.hide()
 	
 func pause (): 
 	get_tree().paused = true 
 	show()
-	
 	$AnimationPlayer.play("blur")
+	pause_pillar_1.hide()
+	pause_pillar_2.hide()
+	pause_icon_pillar.show()
+	
 
 func esc(): 
 	if Input.is_action_just_pressed("esc") and get_tree().paused == false: 
@@ -21,7 +30,9 @@ func esc():
 func _ready() -> void:
 	$AnimationPlayer.play("RESET")
 	hide()
-	
+	pause_pillar_1.show()
+	pause_pillar_2.show()
+	pause_icon_pillar.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -30,6 +41,7 @@ func _process(delta: float) -> void:
 
 func _on_resume_pressed() -> void:
 	resume()
+	
 
 func _on_restart_pressed() -> void:
 	resume()
@@ -38,3 +50,21 @@ func _on_restart_pressed() -> void:
 	print("yea")
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_button_pressed() -> void:
+	if get_tree().paused: 
+		resume() 
+		pause_pillar_1.show()
+		pause_pillar_2.show()
+		pause_icon_pillar.hide()
+
+
+	else: 
+		pause() 
+		pause_pillar_1.hide()
+		pause_pillar_2.hide()
+		pause_icon_pillar.show()
+
+
+	
